44ea58385efb2471f96321303bc3fdc28beec7e7
ismail939 <ismailsalama526@gmail.com>
2023-08-31 14:46:01 +0300

    task to show username beside order

--- First-laravel/resources/views/order/index.blade.php
+++ First-laravel/resources/views/order/index.blade.php
@@ -5,8 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-djjdd
+    <fieldset>
+        <legend>
+            Orders
+        </legend>
+        <table>
+            <thead>
+                <tr>
+                    <th>Price</th>
+                    <th>User Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($orders as $order)
+                    <tr>
+                        <td>{{$order->price}}</td>
+                        <td>{{$order->user->name}}</td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    </fieldset>
 </body>
 </html>
